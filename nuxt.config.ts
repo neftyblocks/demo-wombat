88d9f18ff07cd3dec531d1e091c28620de7b1ff5
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2022-04-26 20:13:02 +0800

    unhide env file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,18 @@
 import { defineNuxtConfig } from 'nuxt';
 
-const chainUrl = 'https://wax.greymass.com:443'
-const appName = 'WombatTest'
-const chainId = '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ['@nefty/ual/module'],
     ual: {
-        appName,
-        chainId,
-        rpcEndpoints: [chainUrl],
+        appName: process.env.APP_NAME,
+        chainId: process.env.CHAIN_ID,
+        rpcEndpoints: [process.env.RPC_ENDPOINT],
     },
     runtimeConfig: {
         public: {
-            APP_NAME: appName,
-            CHAIN_ID: chainId,
-            RPC_ENDPOINTS: [chainUrl],
+            APP_NAME: process.env.APP_NAME,
+            CHAIN_ID: process.env.CHAIN_ID,
+            RPC_ENDPOINTS: [process.env.RPC_ENDPOINT],
         },
     },
     css: ['assets/style.scss'],
